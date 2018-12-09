9f234d66dadc9f9dee2b1115742df3b18c34058d
Yaohongwu <401369870@qq.com>
2018-12-09 21:04:24 +0800

    替换

---
+++ src/com/yaohw/jzoffer/Solution2.java
@@ -0,0 +1,34 @@
+package com.yaohw.jzoffer;
+
+/**
+ * User: 姚宏武
+ * Date: 2018/12/9
+ * Time: 20:43
+ * Email：401369870@qq.com
+ * Description: 请实现一个函数，将一个字符串中的每个空格替换成“%20”。
+ * 例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
+ */
+public class Solution2 {
+
+    public static String replaceSpace(StringBuffer str) {
+
+        StringBuffer buffer = new StringBuffer();
+        char[] chars = str.toString().toCharArray();
+        int i = 0;
+        while (i < chars.length) {
+            if (chars[i] == ' ') {
+                buffer.append("%20");
+            } else {
+                buffer.append(chars[i]);
+            }
+            i++;
+        }
+
+        return buffer.toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "We Are Happy";
+        System.out.println(replaceSpace(new StringBuffer(str)));
+    }
+}
