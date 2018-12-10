f29b175a689f3a7e9abbe2cf86387b930ec43c69
Yaohongwu <401369870@qq.com>
2018-12-10 22:44:02 +0800

    跳台阶,一次可以跳n级

---
+++ src/com/yaohw/jzoffer/Solution9.java
@@ -0,0 +1,19 @@
+package com.yaohw.jzoffer;
+
+/**
+ * User: 姚宏武
+ * Date: 2018/12/10
+ * Time: 22:04
+ * Email：401369870@qq.com
+ * Description:
+ */
+public class Solution9 {
+    public static void main(String[] args) {
+//        System.out.println(JumpFloorII(5));
+        System.out.println(1);
+    }
+
+//    public static int JumpFloorII(int target) {
+//        return 1 * (target - 1);
+//    }
+}
